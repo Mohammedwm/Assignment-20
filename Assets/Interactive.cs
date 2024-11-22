0efbcbb2e97dc43b5922aa1750f4e056418fb4dd
Mohammedwm <mmm81023@gmail.com>
2024-11-22 17:58:59 +0200

    update

--- Assets/Interactive.cs
+++ Assets/Interactive.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Interactive : MonoBehaviour
 {
+    public float deg = 0.0f;
+    public float angleInc = 0.1f;
 
     // Update is called once per frame
     void Update()
     {
-       
+        deg = deg + angleInc;
+        transform.rotation = Quaternion.Euler(new Vector3(deg,0, 0));
     }
 }
